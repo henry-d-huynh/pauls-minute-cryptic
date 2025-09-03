19ae5fc10c306346d422c1f42767b18ae38023d1
Henry Huynh <henry@covr.cc>
2025-09-03 12:55:08 +1000

    Styled the input boxes and fixed clue capitalisation

--- src/components/game-screen/clue/clue.tsx
+++ src/components/game-screen/clue/clue.tsx
@@ -4,7 +4,7 @@ import type { ReactElement } from "react";
 export const GameClue = (): ReactElement => {
   return (
     <div className={styles.clue}>
-      Baby Eats At The Ritz In Carlton’s East Initially but nothing beats a
+      Baby eats at The Ritz in Carlton’s East initially but nothing beats a
       childhood home (8)
     </div>
   );
--- src/components/game-screen/crypt/crypt.tsx
+++ src/components/game-screen/crypt/crypt.tsx
@@ -2,5 +2,18 @@ import styles from "./crypt.module.scss";
 import type { ReactElement } from "react";
 
 export const GameCrypt = (): ReactElement => {
-  return <div className={styles.crypt}>boxes</div>;
+  return (
+    <div className={styles.crypt}>
+      <div className={styles.cryptInput}>
+        <button className={styles.cryptInputButton}></button>
+        <button className={styles.cryptInputButton}></button>
+        <button className={styles.cryptInputButton}></button>
+        <button className={styles.cryptInputButton}></button>
+        <button className={styles.cryptInputButton}></button>
+        <button className={styles.cryptInputButton}></button>
+        <button className={styles.cryptInputButton}></button>
+        <button className={styles.cryptInputButton}></button>
+      </div>
+    </div>
+  );
 };
