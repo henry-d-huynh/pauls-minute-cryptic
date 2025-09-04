037400eae7aba569a808c5f9d6f01701475d3237
Henry Huynh <henry@covr.cc>
2025-09-04 21:47:23 +1000

    Styled Modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -7,8 +7,48 @@ export const Modal = (): ReactElement => {
       <div className={styles.modalOverlay}></div>
       <div className={styles.modalDialog}>
         <div></div>
-        <div>
-          <div>Select a hint</div>
+        <div className={styles.modalDialogBox}>
+          <div className={styles.modalDialogBoxRenderer}>
+            <div className={styles.modalDialogBoxRendererContent}>
+              <div className={styles.modalDialogBoxRendererContentTitle}>
+                Select a hint
+              </div>
+              <div className={styles.modalDialogBoxRendererContentBox}>
+                <div className={styles.modalDialogBoxRendererContentBoxButtons}>
+                  <button
+                    className={
+                      styles.modalDialogBoxRendererContentBoxButtonsButton
+                    }
+                  >
+                    show indicator
+                  </button>
+                  <button
+                    className={
+                      styles.modalDialogBoxRendererContentBoxButtonsButton
+                    }
+                  >
+                    show fodder
+                  </button>
+                  <button
+                    className={
+                      styles.modalDialogBoxRendererContentBoxButtonsButton
+                    }
+                  >
+                    show definition
+                  </button>
+                </div>
+                <div className={styles.modalDialogBoxRendererContentBoxButtons}>
+                  <button
+                    className={
+                      styles.modalDialogBoxRendererContentBoxButtonsButton
+                    }
+                  >
+                    show letter
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
