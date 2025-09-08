3943424aad7d5f47b265d6dcaa69d418beb3d73d
Henry Huynh <henry@covr.cc>
2025-09-08 23:02:46 +1000

    Added highlighting hints

--- src/components/game-screen/clue/clue.tsx
+++ src/components/game-screen/clue/clue.tsx
@@ -1,11 +1,60 @@
 import styles from "./clue.module.scss";
-import type { ReactElement } from "react";
+import { type ReactElement, useState } from "react";
+
+type HintState = "hide" | "show";
 
 export const GameClue = (): ReactElement => {
+  const [indicatorState] = useState<HintState>("hide");
+  const [fodderState] = useState<HintState>("hide");
+  const [definitionState] = useState<HintState>("hide");
+
+  const getIndicatorElement = () => {
+    switch (indicatorState) {
+      case "hide":
+        return <>initially</>;
+      case "show":
+        return <span className={styles.clueIndicator}>initially</span>;
+    }
+  };
+
+  const renderIndicatorElement = getIndicatorElement();
+
+  const getFodderElement = () => {
+    switch (fodderState) {
+      case "hide":
+        return <>Baby eats at The Ritz in Carlton’s East</>;
+      case "show":
+        return (
+          <span className={styles.clueFodder}>
+            Baby eats at The Ritz in Carlton’s East
+          </span>
+        );
+    }
+  };
+
+  const renderFodderElement = getFodderElement();
+
+  const getDefinitionElement = () => {
+    switch (definitionState) {
+      case "hide":
+        return <>childhood home</>;
+      case "show":
+        return <span className={styles.clueDefinition}>childhood home</span>;
+    }
+  };
+
+  const renderDefinitionElement = getDefinitionElement();
+
+  const renderClue = (
+    <>
+      {renderFodderElement} {renderIndicatorElement} but nothing beats a{" "}
+      {renderDefinitionElement}
+    </>
+  );
+
   return (
     <div className={styles.clue}>
-      Baby eats at The Ritz in Carlton’s East initially but nothing beats a
-      childhood home (8)
+      <span>{renderClue}</span> (8)
     </div>
   );
 };
